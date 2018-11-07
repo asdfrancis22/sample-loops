92259ed17ee275e3ade1721865216ea6ad8de49b
Hsien Chih Chen <hsienchihchen@Hsiens-MacBook-Air.local>
2018-11-08 02:49:46 +0800

    added first triangle

--- Loops.java
+++ Loops.java
@@ -1,4 +1,17 @@
 public class Loops{
 	
-	
+	public static void main(String[] args){
+		loop1();
+	}
+
+	static void loop1(){
+		//System.out.println("Loop1");
+		for(int a = 1; a <= 6; a++){
+			System.out.print(a);
+			for(int i=a; i<6; i++){
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
 }
