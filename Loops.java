5769fe474da3a6a3f3dfc75dacfde52da1e5b090
Hsien Chih Chen <hsienchihchen@Hsiens-MacBook-Air.local>
2018-11-08 03:08:21 +0800

    added loops 2 & 3

--- Loops.java
+++ Loops.java
@@ -2,6 +2,8 @@ public class Loops{
 	
 	public static void main(String[] args){
 		loop1();
+		loop2();
+		loop3();
 	}
 
 	static void loop1(){
@@ -13,5 +15,26 @@ public class Loops{
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
+
+	static void loop2(){
+		for (int a=0; a<6; a++){
+			for(int i=(6-a); i>0; i--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	static void loop3(){
+		for (int a=1; a<=6; a++){
+			for(int i=0; i<a; i++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	System.out.println();
 }
